46eb3bd4b00eb5412ab52ebbdefab68a95b419f9
hongochai2702 <hongochai0401@gmail.com>
2018-05-08 07:42:29 +0700

    Update full 2

---
+++ admin/application/language/vi/post_type/portfolio_lang.php
@@ -0,0 +1,119 @@
+<?php
+$lang['text_choose_avatar']          = 'Chọn hình ảnh đại diện';
+$lang['text_insert_multiple']          = 'Chèn nhiều hình';
+$lang['text_selected'] = ' Chọn';
+$lang['heading_title']          = 'Sản phẩm'; 
+
+// Text  
+$lang['text_success']      	 = 'Thành công: Sản phẩm đã được thay đổi!';
+$lang['text_list']              = 'Danh mục Sản phẩm';
+$lang['text_add']               = 'Thêm Sản phẩm';
+$lang['text_edit']              = 'Cập nhật Sản phẩm';
+$lang['text_plus']              = '+';
+$lang['text_minus']             = '-';
+$lang['text_default']           = 'Mặc định';
+$lang['text_image_manager']     = 'Quản lý ảnh';
+$lang['text_browse']            = 'Duyệt tập tin';
+$lang['text_clear']             = 'Xóa Ảnh';
+$lang['text_option']            = 'Tùy chọn';
+$lang['text_option_value']      = 'Giá trị tùy chọn';
+$lang['text_percent']           = 'Tỷ lệ phần trăm';
+$lang['text_amount']            = 'Số lượng Cố định';
+
+// Column
+$lang['column_name']            = 'Tên sản phẩm';
+$lang['column_model']           = 'Dòng Sản phẩm';
+$lang['column_image']           = 'Hình ảnh';
+$lang['column_price']           = 'Giá';
+$lang['column_quantity']        = 'Số lượng';
+$lang['column_status']          = 'Hiện/Ẩn';
+$lang['column_action']          = 'Chỉnh sửa';
+
+// Entry
+$lang['entry_name']             = 'Tên sản phẩm:';
+$lang['entry_meta_keyword']     = 'Meta Tag Keywords';
+$lang['entry_meta_description'] = 'Meta Tag Description';
+$lang['entry_meta_title'] 	     = 'Meta Tag Title';
+$lang['entry_description']      = 'Mô tả sản phẩm:';
+$lang['entry_store']            = 'Cửa hàng:';
+$lang['entry_keyword']          = 'Đường dẫn thân thiện:';
+$lang['entry_filter']           = 'Lọc sản phẩm:';
+$lang['entry_model']            = 'Mã sản phẩm:';
+$lang['entry_sku']              = 'SKU:';
+$lang['entry_upc']              = 'UPC:';
+$lang['entry_ean']              = 'EAN:';
+$lang['entry_jan']              = 'JAN:';
+$lang['entry_isbn']             = 'ISBN:';
+$lang['entry_mpn']              = 'MPN:';
+$lang['entry_location']         = 'Địa chỉ Kho hàng:';
+$lang['entry_manufacturer']     = 'Hãng sản xuất:';
+$lang['entry_shipping']         = 'Yêu cầu Vận chuyển:'; 
+$lang['entry_date_available']   = 'Ngày cập nhật:';
+$lang['entry_quantity']         = 'Số lượng:';
+$lang['entry_minimum']          = 'Số lượng tối thiểu:';
+$lang['entry_stock_status']     = 'Hết hàng';
+$lang['entry_price']            = 'Giá:';
+$lang['entry_tax_class']        = 'Loại Thuế:';
+$lang['entry_points']           = 'Điểm:';
+$lang['entry_option_points']    = 'Điểm:';
+$lang['entry_subtract']         = 'Trừ số lượng Khách đã mua:';
+$lang['entry_weight_class']     = 'Trọng lượng:';
+$lang['entry_weight']           = 'Nặng:';
+$lang['entry_length']           = 'Chiều dài';
+$lang['entry_width']            = 'Chiều rộng';
+$lang['entry_height']           = 'Chiều cao';
+$lang['entry_dimension']        = 'Kích thước <br>(Chiều dài x Chiều rộng x Chiều cao):';
+$lang['entry_image']            = 'Hình ảnh đại diện sản phẩm:';
+$lang['entry_customer_group']   = 'Nhóm khách hàng:';
+$lang['entry_date_start']       = 'Thực hiện Từ ngày:';
+$lang['entry_date_end']         = 'Đến ngày:';
+$lang['entry_priority']         = 'Thứ tự Ưu tiên:';
+$lang['entry_attribute']        = 'Thuộc tính:';
+$lang['entry_attribute_group']  = 'Nhóm thuộc tính:';
+$lang['entry_text']             = 'Văn bản:';
+$lang['entry_option']           = 'Tùy chọn:';
+$lang['entry_option_value']     = 'Giá trị Tùy chọn:';
+$lang['entry_required']         = 'Bắt buộc:';
+$lang['entry_status']           = 'Hiện/Ẩn:';
+$lang['entry_sort_order']       = 'Thứ tự:';
+$lang['entry_category']         = 'Nhóm sản phẩm:';
+$lang['entry_download']         = 'Tập tin có thể tải về:';
+$lang['entry_related']          = 'Sản phẩm cùng nhóm:';
+$lang['entry_tag']              = 'Từ khóa Sản phẩm:';
+$lang['entry_reward']           = 'Điểm thưởng:<br /><span class="help">Điểm cộng tích lũy cho khách hàng. Ví dụ tương ứng với mỗi 10.000Đồng = 1 điểm</span>';
+$lang['entry_layout']           = 'Bố cục:';
+$lang['entry_profile']          = 'Thanh toán Định kỳ:';
+$lang['entry_length_class']     = 'Chiều dài';
+$lang['entry_recurring']        = 'Thanh toán Định kỳ';
+
+// Help
+$lang['help_keyword']           = 'Ví dụ: Dien-thoai-Samsung-Galaxy-S3.html.';
+$lang['help_sku']               = 'Stock Keeping Unit';
+$lang['help_model']               = 'Mã sản phẩm';
+$lang['help_upc']               = 'Mã vạch chuẩn UPC - giới hạn 12 ký tự';
+$lang['help_ean']               = 'Mã vạch chuẩn EAN - giới hạn 14 ký tự';
+$lang['help_jan']               = 'Japanese Article Number';
+$lang['help_isbn']              = 'International Standard Book Number';
+$lang['help_mpn']               = 'Nhà sản xuất Phần số';
+$lang['help_manufacturer']      = 'Nhấp chuột chọn hãng sản xuất';
+$lang['help_minimum']           = 'Số lượng tối thiểu khách phải Mua';
+$lang['help_stock_status']      = 'Hiển thị Trạng thái hết hàng';
+$lang['help_points']            = 'Số Điểm cần để Mua sản phẩm này. Nếu không muốn thanh toán bằng Điểm thì để trống.';
+$lang['help_category']          = 'Click chọn nhóm sản phẩm';
+$lang['help_filter']            = 'Gõ tên Sản phẩm sẽ Tự động hiển thị để lựa chọn';
+$lang['help_download']          = 'Gõ và chọn thêm tên tập tin dữ liệu khách hàng có thể tải về';
+$lang['help_related']           = 'Click để chọn thêm tên Sản phẩm cùng loại. Số lượng tùy ý';
+$lang['help_tag']          	 = 'Ngăn cách mỗi từ Khóa bằng dấu phẩy';
+
+// Error
+$lang['error_warning']          = 'Cảnh báo: Dữ liệu bắt buộc chưa được nhập vào. Kiểm tra các ô trống!';
+$lang['text_warning']          = 'Cảnh báo: Dữ liệu bắt buộc chưa được nhập vào. Kiểm tra các ô trống!';
+$lang['error_permission']       = 'Cảnh báo: Bạn không được phép thay đổi các sản phẩm!';
+$lang['error_name']             = 'Tên sản phẩm phải lớn hơn 3 và nhỏ hơn 255 ký tự!';
+$lang['error_model']            = 'Dòng sản phẩm phải lớn hơn 3 và nhỏ hơn 64 ký tự!'; 
+$lang['error_meta_title']       = 'Từ khóa tóm tắt ít nhất từ 3 đến 32 ký tự';
+$lang['error_keyword']  		 = 'Từ khóa SEO đã được sử dụng!';
+$lang['text_add_fat']  		 = 'Thêm nhanh';
+$lang['text_editfat']  		 = 'SỬA NHANH';
+$lang['text_filter']  		 = 'Lọc theo tên - trạng thái - model - số lượng - giá tiền';
+$lang['text_simple']  		 = 'Ví dụ: dien-thoai-samsung-galaxy-s3.html';
